2e784a1abd4117c6aa2186da5f479625a78788a5
Veelkoov <veelkoov@protonmail.com>
2021-11-12 18:48:54 +0100

    Handle JSON in searches - initial support

--- src/main/resources/vlkv/templates/scripts.js
+++ src/main/resources/vlkv/templates/scripts.js
@@ -46,6 +46,74 @@ function enterInFieldCausesSearch($field) {
     })
 }
 
+function getSearchDataFromHash() {
+    let fallbackResult = {terms: []}
+    let hash = window.location.hash.slice(1)
+
+    if (!hash.startsWith("search:")) {
+        console.log("Search setup: window.location.hash is empty or it doesn't start with `search:`")
+
+        return fallbackResult
+    }
+
+    let decodedData = decodeURI(hash.slice(7));
+    console.log("Search setup: decoded value:", decodedData)
+
+    fallbackResult = {terms: [decodedData]}
+
+    if (!decodedData.startsWith('[') || !decodedData.endsWith(']')) {
+        console.log("Search setup: treating decoded value as a plain string, as it doesn't start with `[` and/or end with `]`")
+
+        return fallbackResult
+    }
+
+    let parsedData
+
+    try {
+        parsedData = JSON.parse(decodedData)
+    } catch (ex) {
+        console.log("Search setup: treating decoded value as a plain string, as it doesn't parse as JSON:", ex.message)
+
+        return fallbackResult
+    }
+
+    if (!parsedData instanceof Array) {
+        console.log("Search setup: treating decoded value as a plain string, as the parsed data is not an array")
+
+        return fallbackResult
+    }
+
+    if (0 === parsedData.length) {
+        console.log("Search setup: skipping, as the parsed data is an empty array")
+
+        return fallbackResult
+    }
+
+    for (let idx in parsedData) {
+        if ('string' !== typeof parsedData[idx]) {
+            console.log(`Search setup: element ${idx} of the decoded array is not a string:`, parsedData[idx])
+
+            return fallbackResult
+        }
+    }
+
+    console.log("Search setup: successfully parsed decoded value")
+
+    return {terms: parsedData}
+}
+
+function setupSearchBasedOnHash() {
+    let searchData = getSearchDataFromHash()
+
+    if (0 === searchData.terms.length) {
+        return
+    }
+
+    // TODO: Handle remaining elements
+
+    $primarySearchField.val(searchData.terms[0])
+}
+
 $(document).ready(function () {
     $checkboxShowIssues = $('#checkboxShowIssues')
     $checkboxShowIssues.on('change', function () {
@@ -102,11 +170,6 @@ $(document).ready(function () {
         $row.next().filter('.issues').data('items', names)
     })
 
-    let hash = window.location.hash.slice(1)
-
-    if (hash.startsWith("search:")) {
-        $primarySearchField.val(decodeURI(hash.substr(7)))
-    }
-
+    setupSearchBasedOnHash()
     refreshVisibility()
 })
