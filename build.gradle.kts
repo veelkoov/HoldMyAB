89888e555a479bdd13c5a61ded2d043396e5cdcc
Veelkoov <veelkoov@protonmail.com>
2022-10-23 23:02:50 +0200

    Some platform updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
     application
     idea
 }
@@ -22,13 +22,13 @@ idea {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-    implementation("io.pebbletemplates:pebble:3.1.5")
+    implementation("io.pebbletemplates:pebble:3.1.6")
 
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.31")
+    testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
 }
 
