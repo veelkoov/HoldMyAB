238cbe00b9f482cb455a753b50c1b792cb0702ad
Veelkoov <veelkoov@protonmail.com>
2023-09-17 14:05:08 +0200

    JVM 17 + build script improvements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
 
     application
     idea
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 group = "vlkv"
@@ -23,11 +25,14 @@ idea {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-    implementation("io.pebbletemplates:pebble:3.1.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
+    implementation("io.pebbletemplates:pebble:3.1.6")
+    implementation("com.github.ajalt.clikt:clikt:4.2.0")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
 
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
@@ -35,10 +40,6 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
 }
 
 application {
